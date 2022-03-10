5bda0e877ff54dc86400a47790aabf4d27e44cfc
Stephanie Lopez <stephanieplopez@gmail.com>
2022-03-10 14:31:20 -0500

    Added menu to Hangman

--- Hangman/app.rb
+++ Hangman/app.rb
@@ -1,3 +1,20 @@
 require_relative 'Board.rb'
 require_relative 'Menu.rb'
 require_relative 'Player.rb'
+
+menu = Menu.new()
+
+while true do
+    menu.display_menu_options()
+    user_selection = gets.chomp
+    if user_selection == "1"
+        puts "User has started game"
+    elsif user_selection == "2"
+        menu.display_exit_message()
+        exit
+
+    # (?) Add try/catch to reject input that is invalid
+    elsif ['exit', 'quit'].any?(user_selection)
+        exit
+    end
+end
