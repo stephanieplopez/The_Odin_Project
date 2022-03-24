70d3448215cca02251c0b2cd4e45b4ea4f0abbfc
Lincoln Guthrie <elink88@gmail.com>
2022-03-24 14:21:51 -0400

    Small changes to file processor

--- Hangman/File_Processor.rb
+++ Hangman/File_Processor.rb
@@ -4,13 +4,11 @@ class File_Processor
     def clean_file()
         contents = File.read('lib/google-10000-english-no-swears.txt')
         valid_words = []
-        contents.each { |word|
-            if word.length >= 5 and word.length <= 12
+        contents.each |word|
+            if word.length() >= 5 and word.length() <= 12
                 valid_words.append(word)
             end
-        } 
         end
         puts "File has been processed and cleaned successfully!"
         #return valid_words
-    end 
-end
+    end
